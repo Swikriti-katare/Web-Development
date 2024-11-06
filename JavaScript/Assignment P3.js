be0a9a8af413bd7bdd194e1c3e38b6b95cde7038
Swikriti katare <swikritikatare27@gmail.com>
2024-11-06 22:47:40 +0530

    added

---
+++ JavaScript/Assignment P3.js
@@ -0,0 +1,67 @@
+// Question 1
+
+/*let arr = [1,2,3,4,5];
+let n= 3;
+let result = arr.slice(0, n);
+console.log(result);
+
+// Question 2
+
+let result1 = arr.splice(2);
+console.log(result1);
+
+  // Question 3
+
+let str = prompt("My name is Swikriti");
+if(str.length == 0)
+    {
+        console.log("The String is empty");
+    }
+else{
+        console.log("The string is not empty");
+}
+
+   // Question 4
+
+    let str = ("sWiKrItI kAtArE");
+    i = 4;
+    if(str[i] == str[i].toLowerCase)
+        {
+            console.log("it is lowercase");
+        }
+    else{
+        console.log("it is not lowercase");
+    }
+
+    //Question 5
+
+let str = prompt("Enter your string");
+console.log = (`the entered string is ${str}`);
+console.log = (`the trimed str is ${str.trim()}`);
+
+ // Question 6
+
+let arr = [ 1, 2, 3 ,4, -9];
+let i = 8
+if(arr.indexOf(i) == -1)
+    {
+        console.log("does not exist");
+    }
+else {
+    console.log("exist");
+}
+
+// print Odd nnumbers from 1 - 15
+
+for(let i=1; i<=15; i=i+2)
+    {
+        console.log(i);
+    }*/
+
+// print even numbers from 1-15
+
+for(let i=0; i<=15; i=i+2)
+    {
+        console.log(i);
+    }
+
